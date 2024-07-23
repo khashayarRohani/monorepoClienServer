ed74dda84b3484d94e1b04324474ad6a71d61375
khashayarRohani <khashayarrhi@gmail.com>
2024-07-23 15:54:19 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", function (req, res) {
+  res.json("Welcome to Api");
+});
+
+app.post("/joke", function (request, response) {
+  // retrieve the information from the form
+  console.log(request.body);
+
+  response.json("Thank you for making a new entry");
+});
+
+app.listen(8181, function (req, res) {
+  console.log("you are listening to port 8181");
+});
